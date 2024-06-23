aec7501fce689f32963369fe8f4f48e0b60681a0
Yoann Auroy <yoann.ar.pro@gmail.com>
2024-06-23 10:43:12 +0200

    changing client method

--- app/client.js
+++ app/client.js
@@ -1,14 +1,22 @@
-const { Pool } = require("pg");
-const pool = new Pool({
-  user: process.env.BD_USER,
-  host: process.env.DB_HOSTNAME,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+// const { Pool } = require("pg");
+// const pool = new Pool({
+//   user: process.env.BD_USER,
+//   host: process.env.DB_HOSTNAME,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 
-pool.on("error", (err, client) => {
-  console.error("Unexpected error on idle client", err);
-});
+// pool.on("error", (err, client) => {
+//   console.error("Unexpected error on idle client", err);
+// });
 
-module.exports = pool;
+// module.exports = pool;
+
+const { Client } = require("pg");
+
+const client = new Client(process.env.PG_URL);
+
+client.connect();
+
+module.exports = client;
